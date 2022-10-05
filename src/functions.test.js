ce6ad2b1c8b1263ecf7b3806b4dd1d4d220c9b5f
amon-cofie <ddd.amon123@gmail.com>
2022-10-05 12:42:28 +0000

    Add test for remove function

--- src/functions.test.js
+++ src/functions.test.js
@@ -1,7 +1,15 @@
-import { addTask } from './functions.js';
+import { addTask, removeTask } from "./functions.js";
 
-it('Add function test', () => {
-  const arr = [];
-  addTask(arr, 'string');
-  expect(arr.length).toBe(1);
+describe("Test the remove function", () => {
+  test("Add function test", () => {
+    const arr = [];
+    addTask(arr, "string");
+    expect(arr.length).toBe(1);
+  });
+
+  test("Remove function test", () => {
+    const arr = [{ a: 1 }, { b: 2 }, { c: 3 }];
+    removeTask(arr, 1);
+    expect(arr.length).toBe(2);
+  });
 });
